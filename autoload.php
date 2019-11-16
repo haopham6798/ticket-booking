e845befad0a256bd4f5e1e707191e82fbf7715bb
haopham6798 <haopham6798@gmail.com>
2019-11-17 04:03:03 +0700

    first commit

---
+++ autoload.php
@@ -0,0 +1,22 @@
+<?php
+
+spl_autoload_register(function ($class){
+	$prefix = "WS\\ticket-booking\\";
+	$base_dir = __DIR__ . '/src/';
+	
+	$len = strlen($prefix);
+	
+	if(strncmp($prefix,$class,$len) !== 0){
+		return;
+	}
+	echo "loadingg....";
+	$relative_class = substr($class,$len);
+	
+	$file = $base_dir . str_replace("\\","/",$relative_class) . ".php";
+	
+	if(file_exists($file)){
+		require $file;
+	}
+})
+
+?>
