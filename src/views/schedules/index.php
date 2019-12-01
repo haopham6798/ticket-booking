4562b63aef8aaedc82e1fe5c2fe369cc45535846
root <loclion17@gmail.com>
2019-12-01 20:43:15 +0700

    adad

--- src/views/schedules/index.php
+++ src/views/schedules/index.php
@@ -1,74 +1,34 @@
 <div class="container">
-    <?php
-        
-        foreach($schedules as $schedule) {
-            
-        }
-        $days = array_unique($days);
-        $sche_movies = array_unique($sche_movies);
-        print_r($sche_movies);
-        $uses = [];
+    <table class="table">
+        <tr>
+            <th>  Schedule ID </th>
+            <th>  Time Start  </th>
+            <th>  Cinema</th>
+            <th>  movie </th>
+        </tr>
+        <?php
+            foreach($schedules as $schedule) {
+        ?>
+        <tr>
 
-        foreach($days as $day) {
-            // print_r($day);
-            $mov = [];
-            foreach ($sche_movies as $sche_movie) {
-                $time = [];
-                foreach($sches as $sche) {
-                    if ($day == $sche[0]) {
-                        $time[] = $sche[1];
-                    }
-                }
+            <td> <?php  echo $schedule->schedule_id ?> </td>
+            <td>  <?php  echo $schedule->schedule_time_start ?>  </td>
+            <td>  <?php  echo $schedule->cinema_id ?> </td>
+            <td>  <?php  echo $schedule->movie_name ?> </td>
+            <td> 
+                <a href="index.php?controller=schedules&action=delete&schedule_id=<?php  echo $schedule->schedule_id ?>"><button
+                    class="btn btn-secondary">Delele</button></a>
+                <a href="index.php?controller=schedules&action=update&schedule_id=<?php  echo $schedule->schedule_id ?>"><button
+                    class="btn btn-secondary">Update</button></a>
+            </td>
+        </tr>
+
+        <?php
             }
-            
-            $uses[] = array($day => $time);
-        }
-
-        // print_r($uses[0]);
-
-        foreach($schedules as $schedule) {
-    ?>
-        <a href="index.php?controller=schedules&date=<?php echo explode(" ", $schedule->schedule_time_start)[0]; ?>">
-
-            <button id="btn-schedule" type="button" class="btn btn-outline-secondary" value=<?php echo explode(" ", $schedule->schedule_time_start)[0]; ?>>
-                <?php  echo explode(" ", $schedule->schedule_time_start)[0]; ?>
-            </button>
-        </a>
-    <?php
-        }
-    ?>
-
-    <hr>
-    <br><br>
-
-    <?php
-    if (isset($_GET['date']))  {
-
-        $date = $_GET['date'];
-        // print_r($date);
-        foreach($uses as $use) {
-            // print_r($use);
-            if (key($use) == $date ) {
-                // print("aco");
-                // print_r($use);
-                foreach ($use[$date] as $time) {
-    ?>
-        <a href="index.php?controller=tickets&movie_id=<?php 
-                echo $schedule->movie_id ?>&date=<?php echo key($use); ?>&time=<?php echo($time)  ?>">
-            <button type="button" class="btn btn-secondary" >
-                <?php echo($time); ?>
-            </button>
-        </a>
-
-    <?php
-                }
-            }
-        }
-    }
-    else  {
         
-    }
-    ?>
+        ?>
+
+    </table>
     
 </div>
 
